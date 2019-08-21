f99bce814df1dc1f3ea933764b980d358fa8322c
gka <gka@users.noreply.github.com>
2019-08-21 03:42:02 +0200

    cleaned up R code

--- co2-history.R
+++ co2-history.R
@@ -14,23 +14,46 @@
 # library(needs)
 needs(dplyr, ggplot2, readr, grid, extrafont) # replace with `library(...)` if you don't like needs
 
-font_import()
+# font_import()
 loadfonts(device = "postscript")
-
-co2 <- read_tsv('co2.tsv')
 family <- 'Lato'  # change to whatever font you like :)
 
-wrp <- function(year) {
-  ifelse(year < 1900, (2019-year)^warp, (2019-1900)^warp + (1900-year)*0.005)
-}
 
+# a custom ggplot function for text annotations with vertical line
+# see https://yutani.rbind.io/post/2017-11-07-ggplot-add/
 geom_text_annotation <- function(label, yr, origin, offset) {
   structure(list(label=label, year=yr, origin=origin, offset=offset), class = "text_annotation")
 }
+ggplot_add.text_annotation <- function(object, plot, object_name) {
+  numLines <- length(strsplit(object$label, '\n')[[1]] )
+  l1 <- annotate('text',
+                 label=object$label,
+                 x=warp(object$year)+0.02,
+                 y=object$origin+object$offset,
+                 size=3.7,
+                 family=family,
+                 lineheight = 0.8,
+                 vjust=ifelse(object$offset<0,1,-1.5+numLines*0.32),
+                 hjust = 0)
+  l2 <- geom_segment(aes(x=warp(object$year),
+                         xend=warp(object$year),
+                         y=object$origin + object$offset + ifelse(object$origin<0,-4,+4),
+                         yend=object$origin),
+                     size=0.2,
+                     linetype=2,
+                     color='#aaaaaa')
+  plot$layers <- append(plot$layers, l1)
+  plot$layers <- append(plot$layers, l2)
+  plot
+}
 
+# here's the time-warp function
+warp <- function(year) {
+  warp.factor <- 0.15
+  ifelse(year < 1900, (2019-year)^warp.factor, (2019-1900)^warp.factor + (1900-year)*0.005)
+}
 
-warp <- 0.15
-
+# need to add custom ticks to get real years on axis
 ticks <- c(-8e5,-6e5,-4e5,-3e5,-2e5,-1e5,-4e4,-2e4,-1e4,-5e3,-2e3, 0,1000,1500,1900, 2019)
 labels <- sapply(ticks, function(y) {
   if (y == -8e5) {
@@ -42,35 +65,16 @@ labels <- sapply(ticks, function(y) {
   }
 })
 
-ggplot_add.text_annotation <- function(object, plot, object_name) {
-  numLines <- length(strsplit(object$label, '\n')[[1]] )
-  l1 <- annotate('text',
-                 label=object$label,
-                 x=wrp(object$year)+0.02,
-                 y=object$origin+object$offset,
-                 size=3.7,
-                 family=family,
-                 lineheight = 0.8,
-                 vjust=ifelse(object$offset<0,1,-1.5+numLines*0.32),
-                 hjust = 0)
-  l2 <- geom_segment(aes(x=wrp(object$year),
-                         xend=wrp(object$year),
-                         y=object$origin + object$offset + ifelse(object$origin<0,-4,+4),
-                         yend=object$origin),
-                     size=0.2,
-                     linetype=2,
-                     color='#aaaaaa')
-  plot$layers <- append(plot$layers, l1)
-  plot$layers <- append(plot$layers, l2)
-  plot
-}
 
+# load data file
+co2 <- read_tsv('co2.tsv')
 
+# plot and annotate
 fig <- co2 %>% 
-  mutate(year.ago=wrp(year)) %>%
-  ggplot(aes(x=year.ago, y=co2ppm)) +
+  mutate(year.warped=warp(year)) %>%
+  ggplot(aes(x=year.warped, y=co2ppm)) +
   geom_line(size=0.7, color='#1d81a2') +
-  scale_x_reverse(breaks=wrp(ticks), labels=labels) +
+  scale_x_reverse(breaks=warp(ticks), labels=labels) +
   xlab('years ago') +
   theme_minimal() +
   theme(text=element_text(size=12, family=family),
@@ -91,15 +95,18 @@ fig <- co2 %>%
   geom_text_annotation('World\nWar II', 1945, 308,-20) +
   geom_text_annotation('World\nWide\nWeb', 1989, 350,-20) +
   geom_text_annotation('Bitcoin', 2009, 386,-20) +
-  annotate('text', wrp(-8e5), 400,
+  annotate('text', warp(-8e5), 400,
            hjust=0, vjust=2,size=8, parse=T,
            family=family, lineheight=1,
            label='bold("CO2 concentration in atmosphere\nover last 800,000 years")') +
-  labs(lineheight=1.4,color='#999999',caption='Time is warped using sqrt scale before 1900 for readability. Graphic: Gregor Aisch, vis4.net\nSource: NOAA (1959-today, 1800-1850), NASA (1850-1958), Monnin et al., Petit et al., Siegenthaler et al., Luethi et al. (pre 1800)') 
-  
+  labs(lineheight=1.4,color='#999999',
+       caption='Time is warped using sqrt scale before 1900 for readability. Graphic: Gregor Aisch, vis4.net
+       Source: NOAA (1959-today, 1800-1850), NASA (1850-1958), Monnin et al., Petit et al., Siegenthaler et al., Luethi et al. (pre 1800)') 
+
 print(fig)
 
-ggsave('co2-history.png', fig, device='png', width=14,height=7, dpi=300, scale=1)
+# save plot as PNG and PDF
+ggsave('co2-history.png', fig, device='png', width=14,height=7, dpi=150, scale=1)
 
 cairo_pdf("co2-history.pdf", width=14, height = 7)
 print(fig)
